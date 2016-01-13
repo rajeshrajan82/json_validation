ae43b7207f51a3b22f9d7727942deecc969f61d9
Raj <rajesh@techhoppers.com>
2016-01-14 00:33:21 +0530

    Parsing the json value and display the out put

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,14 @@
 class WelcomeController < ApplicationController
+  require 'json'
+  
+  def json_parser
+    respond_to do |format|
+      begin
+        @resume = JSON.parse params[:json_text]
+      rescue
+        format.html { redirect_to root_path, :notice => "Invalid json data"}
+      end
+      format.html { render :resume }
+    end      
+  end
 end
